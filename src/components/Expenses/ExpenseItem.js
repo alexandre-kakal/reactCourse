b74624e0d762867cb8289afde621cf09d7822faa
alexandre-kakal <alexandre.ls.kakal@gmail.com>
2022-08-04 19:57:55 +0200

    feat : add states in order to change components titles

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import React, { useState } from "react";
+
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHander = () => {
+    setTitle("Uptdated !");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHander}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
